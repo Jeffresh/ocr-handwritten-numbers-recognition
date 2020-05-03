f7681595538b404178dbfa0d473d1ffe8ec8f4f7
Jeff <jeff.uca@gmail.com>
2020-05-03 14:36:04 +0200

    :sparkles: add function to obtain representative of each class

---
+++ meanFeatures.m
@@ -0,0 +1,14 @@
+function means = meanFeatures(x,y)
+%MEANFEATURES obtain 
+% 
+
+if min(y) == 0; classesNumber = max(y) +1 ; else classNumber = max(y); end
+
+means = zeros(classesNumber,size(x,2))
+    for i=1 :classesNumber
+        index= y==i-1;
+        means(i,:) = mean(x(index,:));
+    end
+
+end
+
