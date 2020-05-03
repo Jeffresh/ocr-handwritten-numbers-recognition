c3d83b302cdd9e92d4108e6277fb8753da5a4129
Jeff <jeff.uca@gmail.com>
2020-05-03 18:48:59 +0200

    :recycle: add semicolon to avoid prints on console

--- meanFeatures.m
+++ meanFeatures.m
@@ -4,7 +4,8 @@ function means = meanFeatures(x,y)
 
 if min(y) == 0; classesNumber = max(y) +1 ; else classNumber = max(y); end
 
-means = zeros(classesNumber,size(x,2))
+means = zeros(classesNumber,size(x,2));
+
     for i=1 :classesNumber
         index= y==i-1;
         means(i,:) = mean(x(index,:));
