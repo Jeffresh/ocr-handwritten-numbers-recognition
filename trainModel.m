a6a8702d7c14b57de02a6dc43b8bc6e9f6ab7b93
Jeff <jeff.uca@gmail.com>
2020-05-13 19:37:33 +0200

    :fire: delete file trainModel
    revamped code in train file with meanFeatures

--- trainModel.m
+++
@@ -1,9 +0,0 @@
-function model = trainModel(x,y)
-%TRAINMODEL gets the model trained using minimun distance
-
-if min(y) == 0; classesNumber = max(y) +1 ; else classNumber = max(y); end
-means = meanFeatures(x, y);
-model = means;
-
-end
-
