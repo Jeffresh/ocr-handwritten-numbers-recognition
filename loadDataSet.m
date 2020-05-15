a95df5f0bd0b21db3304c1936bf65914066e1d15
Jeff <jeff.uca@gmail.com>
2020-05-15 20:53:35 +0200

    :recycle: now loadDataset uses extractFeatures

--- loadDataSet.m
+++ loadDataSet.m
@@ -2,7 +2,7 @@ function [xtrn,ytrn] = loadDataSet()
 %LOADDATASET return data set splited into to variables, xtrain and ytrain
 dataset = load('data_mnist_students');
 
-xtrn = dataset.xtrn';
+xtrn = extractFeatures(dataset.xtrn)';
 ytrn = dataset.ytrn';
 
 end
