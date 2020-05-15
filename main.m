da93f792f428793e955cb0c346c812425bde7972
Jeff <jeff.uca@gmail.com>
2020-05-15 20:58:32 +0200

    :sparkles: add get mean error using kv

--- main.m
+++ main.m
@@ -2,7 +2,26 @@ clear all
 
 [xtrn, ytrn] = loadDataSet();
 
-%%
+
+%% Mean error using KV
+
+number_segments = 23;
+ 
+indexes_partitions = get_indexs_kv_partitions(xtrn,number_segments);
+
+
+for j=1:19
+    
+    [x_train,y_train, x_test,y_test] =  get_kv_partition(xtrn,ytrn,indexes_partitions,j);
+    model = train(x_train,y_train);
+    prediction = evalue(x_test, model);
+    wcp(j) = costFunction(prediction,y_test);
+end
+
+meanError = mean(wcp)
+
+%% Error using same set to train and evalue
+
 clear model prediction error
    
 model = train(xtrn,ytrn);
